79251b1a936aece167b9b94b56e5fa8350817db2
RohitKSahoo <rohitkumarsahoo37@gmail.com>
2026-04-22 00:21:00 +0530

    automated repository maintenance | 2026-04-22 00:21:00

--- src/models/Vendor.ts
+++
@@ -1,9 +0,0 @@
-export interface Vendor {
-  id?: string;
-  auth_uid?: string;
-  name: string;
-  phone: string;
-  location: string;
-  verified: boolean;
-  created_at: Date | string | number; // Support Firestore Timestamp or ISO string
-}
